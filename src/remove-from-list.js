fb7938cf29fa215f5af5708170f22a0724caab85
Vitaly Undro <vitalyundro@gmail.com>
2022-05-01 18:07:20 +0300

    fit: complete task 'remove-from-list'

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let current = l;
+  let prev;
+
+  while (current) {
+    if (current.value === k) {
+      prev ? (prev.next = current.next) : (l = current.next);
+      current = l;
+    }
+    prev = current;
+    current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
